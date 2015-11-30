1e895dca752d89a4f01c960517dbe1f8dbdf1754
Aleksander Kwiatkowski <bobikx@poczta.fm>
2015-11-30 14:00:53 +0100

    another cam

--- src/start_archive.cr
+++ src/start_archive.cr
@@ -4,7 +4,6 @@ require "../src/webcam_downloader"
 # TODO
 # flattach
 # lorenzalm
-# lienz - !!!
 # kronplatz - !!!
 # gantkofel - !!!
 # meran - !!!
@@ -29,10 +28,13 @@ require "../src/webcam_downloader"
 # http://www.foto.webcam/suedtirol/vinschgau/mals/hotel-watles/12/webcam?date=2015/11/26/0240
 # http://www.foto.webcam/suedtirol/groeden/st-christina/dorfhotel-beludei/6/webcam?date=2015/11/26/2040
 
+# DONE
+# DONE full 2015 - lienz - !!!
+
 w = WebcamDownloader::ArchiveDownloader.new
 w.logger.level = Logger::DEBUG
 w.server_host = "http://www.foto-webcam.eu/"
 w.server_webcam_path = "webcam/"
 w.server_list_path = "webcam/include/list.php"
-w.name = "lienz"
+w.name = "ankogel-sued"
 w.make_it_so
