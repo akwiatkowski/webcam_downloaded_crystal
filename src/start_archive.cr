ddc92ecd3c2f10d12d11ef51f5568b8bceed958b
Aleksander Kwiatkowski <bobikx@poczta.fm>
2015-12-01 10:38:23 +0100

    fix

--- src/start_archive.cr
+++ src/start_archive.cr
@@ -7,7 +7,6 @@ require "../src/webcam_downloader"
 # kronplatz - !!!
 # gantkofel - !!!
 # meran - !!!
-# NOW ankogel-sued - !!!
 # schareck - !
 # gletscher-sued - !
 # obervellach - !!
@@ -28,25 +27,33 @@ require "../src/webcam_downloader"
 # http://www.foto.webcam/suedtirol/vinschgau/mals/hotel-watles/12/webcam?date=2015/11/26/0240
 # http://www.foto.webcam/suedtirol/groeden/st-christina/dorfhotel-beludei/6/webcam?date=2015/11/26/2040
 
+# NOW ankogel-sued - !!!
+
 # DONE
 # DONE full 2015 - lienz - !!!
+
+download_hd = true
 
 w = WebcamDownloader::ArchiveDownloader.new
 w.logger.level = Logger::DEBUG
 w.server_host = "http://www.foto-webcam.eu/"
 w.server_webcam_path = "webcam/"
 w.server_list_path = "webcam/include/list.php"
-w.name = "lienz"
+w.name = "ankogel-sued"
 
 # downloading hd
-w.format = "hd"
-w.resize = false
+if download_hd == true
+  w.format = "hd"
+  w.resize = false
+end
 
 # downloading full
-w.format = "fu"
-w.sleep_between_lists = 10
-w.sleep_between_image_download = 10
-w.resize = true
-w.resize_jpeg_quality = 85
+if download_hd == false
+  w.format = "fu"
+  w.sleep_between_lists = 10
+  w.sleep_between_image_download = 10
+  w.resize = true
+  w.resize_jpeg_quality = 85
+end
 
 w.make_it_so
